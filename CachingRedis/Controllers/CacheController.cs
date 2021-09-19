8c0bc7f196189f5e51d7ec9b617985dbd1fcaeb2
Emon <Emon@DESKTOP-98TRJS2>
2021-09-19 12:57:10 +0600

    In memory caching

--- CachingRedis/Controllers/CacheController.cs
+++ CachingRedis/Controllers/CacheController.cs
@@ -24,20 +24,37 @@ namespace CachingRedis.Controllers
             memoryCache.TryGetValue(key, out value);
             return Ok(value);
         }
+
+
+        //{
+        //    "key":"Name",
+        //    "value": "Emon"
+        //}
+
         [HttpPost]
         public IActionResult SetCache(CacheRequest data)
         {
             var cacheExpiryOptions = new MemoryCacheEntryOptions
             {
                 AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                //Absolute Expiration – The problem with Sliding Expiration is that theoretically, it can last forever. Let’s say someone requests for the data every 1.59 minutes for the next couple of days, the application would be technically serving an outdated cache for days together. With Absolute expiration, we can set the actual expiration of the cache entry. Here it is set as 5 minutes.So, every 5 minutes, without taking into consideration the sliding expiration, the cache will be expired.It’s always a good practice to use both these expirations checks to improve performance. But remember, the absolute expiration SHOULD NEVER BE LESS than the Sliding Expiration.
+
                 Priority = CacheItemPriority.High,
+                //Priority – Sets the priority of keeping the cache entry in the cache.The default setting is Normal.Other options are High,Low and Never Remove.This is pretty self - explanatory.
                 SlidingExpiration = TimeSpan.FromMinutes(2),
+                //Sliding Expiration – A defined Timespan within which a cache entry will expire if it is not used by anyone for this particular time period.In our case, we set it to 2 minutes.If, after setting the cache, no client requests for this cache entry for 2 minutes, the cache will be deleted.
+
                 Size = 1024,
+                //Size – Allows you to set the size of this particular cache entry, so that it doesn’t start consuming the server resources.
+
             };
             memoryCache.Set(data.key, data.value, cacheExpiryOptions);
             return Ok();
         }
-        public class CacheRequest
+
+
+
+    public class CacheRequest
         {
             public string key { get; set; }
             public string value { get; set; }
--- CachingRedis/Startup.cs
+++ CachingRedis/Startup.cs
@@ -1,7 +1,9 @@
+using CachingRedis.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,10 @@ namespace CachingRedis
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+           options.UseSqlServer(
+               Configuration.GetConnectionString("DefaultConnection"),
+               b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
             services.AddControllers();
 
